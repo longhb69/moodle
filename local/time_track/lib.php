8ed4474f9268c578875d5d658b7a7b4b8409b35e
longhb69 <thanhlongh124@gmail.com>
2025-02-20 20:31:50 +0700

    first commit

---
+++ local/time_track/lib.php
@@ -0,0 +1,7 @@
+<?php
+
+function local_time_track_before_footer() {
+    global $PAGE;
+
+    $PAGE->requires->js_call_amd('local_time_track/tracker', 'init')
+}
---
+++ local/time_track/version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_time_track';
+$plugin->version = 2025021600;
+$plugin->requires = 2021051700;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';

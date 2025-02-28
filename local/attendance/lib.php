f9d6ffca82cafe70d615c10dbf79858697ebc365
Long <thanhlongh124@gmail.com>
2025-02-28 16:51:52 +0700

    atte

--- local/attendance/lib.php
+++ local/attendance/lib.php
@@ -9,7 +9,14 @@ function local_myplugin_extend_navigation(global_navigation $nav) {
 
 function get_attendance_data($userid) {
     global $DB;
-    $sql = "SELECT * FROM {lesson_time_tracking} Where userid = :userid";
+    $sql = "SELECT ltt.id, userid, login, logout, classid, c.fullname as coursename, l.name as lessonname, timespent
+                FROM {lesson_time_tracking} ltt
+                JOIN {lesson} l
+                ON ltt.lessonid = l.id
+                JOIN {course} c
+                ON ltt.courseid = c.id
+                Where userid = :userid"
+            ;
     $params = ['userid' => $userid];
     $records = $DB->get_records_sql($sql, $params);
     $attendance = [];
@@ -18,14 +25,31 @@ function get_attendance_data($userid) {
         return [];
     }
 
+    $weekdays = [
+        'Monday' => 'T2',
+        'Tuesday' => 'T3',
+        'Wednesday' => 'T4',
+        'Thursday' => 'T5',
+        'Friday' => 'T6',
+        'Saturday' => 'T7',
+        'Sunday' => 'CN'
+    ];
+
     foreach ($records as $record) {
+        $login_day_name = strftime('%A', $record->login);
+        $logout_day_name = strftime('%A', $record->logout);
+        $login_day_short = $weekdays[$login_day_name] ?? $login_day_name;
+        $logout_day_short = $weekdays[$logout_day_name] ?? $logout_day_name;
+        $login = $login_day_short . ', ' . userdate($record->login, '%d/%m/%Y, %I:%M%p');
+        $logout = $logout_day_short . ', ' . userdate($record->logout, '%d/%m/%Y, %I:%M%p');
+
         $attendance[] = [
             'userid' => $record->userid,
-            'lessonid' => $record->lessonid,
-            'login' => userdate($record->login),
-            'logout' => userdate($record->logout),
+            'lessonname' => $record->lessonname,
+            'login' => $login,
+            'logout' => $logout,
             'classid' => $record->classid,
-            'courseid' => $record->courseid,
+            'coursename' => $record->coursename,
             'timespent' => gmdate("H:i:s", $record->timespent)
         ];
     }
